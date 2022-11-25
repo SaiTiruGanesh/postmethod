9f1e3e0107bf57efc7ad281156bb5aaf18fa52e3
Savithriganta257 <savithriganta2003@gmail.com>
2022-11-25 13:19:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=3000
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get("/",(req,res)=>{
+	res.send("server is yet yet to be started")
+})
+
+app.post("/newData",(req,res)=>{
+	console.log(req.body)
+	const {name,password}=req.body
+	res.send("Added succesfully")
+})
+
+app.listen(port,()=>console.log("server is running on port 3000"))
